ec563276c4225efa3718caa73f7d5e8f5608efaa
Emoji <studyemoji@mail.ru>
2024-04-07 19:09:48 +0300

    added article.css, design complete oh forget archive in aside, ooops

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,20 +4,35 @@ import '../assets/styles/main.css';
 import '../assets/styles/index.css';
 const Main = () => {
     const articles = [
-        { title: 'Первая статья', category: 'Общество', author: 'Иван Иванов', content: 'Содержание первой статьи' },
-        { title: 'Вторая статья', category: 'Технологии', author: 'Петр Петров', content: 'Содержание второй статьи' },
-        // Добавьте другие статьи по аналогии
+        {
+            title: 'Марк Цукерберг продемонстрировал работу VR-гарнитуры Project\n' +
+                'Cambria',
+            category: 'Технологии',
+            author: 'Иван Иванов',
+            content: 'Глава Meta Марк Цукерберг опубликовал в видео работы VR-гарнитуры нового\n' +
+                'поколения, поддерживающей приложения виртуальной и дополненной реальности. На\n' +
+                'этапе разработки устройство носит название Project Cambria, а его выход может состояться\n' +
+                'уже в этом году.' },
+        {
+            title: 'Вторая статья',
+            category: 'Технологии',
+            author: 'Петр Петров',
+            content: 'Содержание второй статьи' },
     ];
 
   return (
       <main className="main">
           <h2 className="main-title">Записи</h2>
           <hr className="line"/>
-          <div className="articles">
+          <div className="articles-container">
               {articles.map(article => (
-                  <Article title={article.title} category={article.category} author={article.author} content={article.content} />
+                  <Article
+                      title={article.title}
+                      category={article.category}
+                      author={article.author}
+                      content={article.content}
+                  />
               ))}
-              {/*    TODO: Тут статьи */}
           </div>
       </main>
   );
--- src/elements/Article.js
+++ src/elements/Article.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import '../assets/styles/article.css';
 
 const Article = ({ title, category, author, content}) => {
     return (
         <article className="article_container">
             <h1 className="article-title">{title}</h1>
-            <p className="article-content">{category}</p>
-            <p className="article-content">{author}</p>
+            <p className="article-category">Категория: <strong>{category}</strong></p>
+            <p className="article-author">Автор: <strong>{author}</strong></p>
             <p className="article-content">{content}</p>
+            <div className="buttons-container">
+                <button className="delete-button" onClick={handleDelete}>
+                    Удалить
+                </button>
+                <button className="archive-button" onClick={handleArchive}>
+                    Архив
+                </button>
+            </div>
         </article>
     );
 };
+
+function handleDelete() {
+}
+function handleArchive() {
+}
 
 export default Article;
